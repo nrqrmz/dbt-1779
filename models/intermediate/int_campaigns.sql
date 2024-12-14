f0a45a76d648183bd672edaf4840945606e344ad
Enrique Ramirez Ramos <nrqrmz@gmail.com>
2024-12-14 09:33:49 +0000

    add reason why the file was refactored in a comment

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -15,3 +15,5 @@
         relations=[ref('stg_raw__adwords'), ref('stg_raw__bing'), ref('stg_raw__criteo'), ref('stg_raw__facebook')]
     )
 }}
+
+-- This file was refactored using a library instead union all
